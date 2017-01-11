84c5789644ceefa6a0497d004fd0889b42af6664
Chino Kim <chjno.k@gmail.com>
2017-01-11 02:07:12 -0500

    +switch

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ function listen(){
   var port = server.address().port;
   console.log('Example app listening at http://' + host + ':' + port);
 }
- 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
-});
- 
+
+var lswitch = {};
+var light = {};
 client.on('connect', function(connection) {
   console.log('WebSocket Client Connected');
+
   connection.on('error', function(error) {
       console.log("Connection Error: " + error.toString());
   });
@@ -23,29 +22,43 @@ client.on('connect', function(connection) {
       console.log('echo-protocol Connection Closed');
   });
   connection.on('message', function(message) {
-      if (message.type === 'utf8') {
-          console.log("Received: '" + message.utf8Data + "'");
-      }
-  });
 
-  var stdin = process.openStdin();
-  stdin.addListener("data", function(d) {
-    var input = d.toString().trim();
-    if (input == '0'){
-      connection.sendUTF('0');
-    } else if (input == '1'){
-      connection.sendUTF('1');
+    if (connection == lswitch){
+      console.log(message.utf8Data);
+      if (light.connected){
+        if (message.utf8Data == '0'){
+          light.sendUTF('0');
+        } else if (message.utf8Data == '1'){
+          light.sendUTF('1');
+        }
+      }
+    } else {
+      if (message.utf8Data == 'switch'){
+        console.log('switch connected');
+        lswitch = connection;
+      } else if (message.utf8Data == 'light'){
+        console.log('light connected');
+        light = connection;
+      }
     }
   });
-  
-  // function sendNumber() {
-  //     if (connection.connected) {
-  //         var number = Math.round(Math.random() * 0xFFFFFF);
-  //         connection.sendUTF(number.toString());
-  //         setTimeout(sendNumber, 1000);
-  //     }
-  // }
-  // sendNumber();
+
+  if (light.connected){
+    var stdin = process.openStdin();
+    stdin.addListener("data", function(d) {
+      var input = d.toString().trim();
+      if (input == '0'){
+        light.sendUTF('0');
+      } else if (input == '1'){
+        light.sendUTF('1');
+      }
+    });
+  }
+});
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
 });
  
-client.connect('ws://172.22.6.89:3002/');
+// client.connect('ws://172.22.6.89:3002/');
+client.connect('ws://172.22.6.89:3001/');
