ed619dde257bb0a71db12689ae52c025a5ca6696
Chino Kim <chjno.k@gmail.com>
2017-01-11 00:47:11 -0500

    working ws server

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 var express = require('express');
 var app = express();
 var server = app.listen(3000, listen);
-var request = require('request');
+var WebSocketClient = require('websocket').client;
+var client = new WebSocketClient();
 
 function listen(){
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://' + host + ':' + port);
 }
-
-app.get('/on', function (req, res) {
-  console.log('switch on');
+ 
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
 });
-
-app.get('/off', function (req, res) {
-  console.log('switch off');
-});
-
-var stdin = process.openStdin();
-stdin.addListener("data", function(d) {
-  var input = d.toString().trim();
-  if (input == '0'){
-    request('http://172.22.6.89/off', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
+ 
+client.on('connect', function(connection) {
+  console.log('WebSocket Client Connected');
+  connection.on('error', function(error) {
+      console.log("Connection Error: " + error.toString());
+  });
+  connection.on('close', function() {
+      console.log('echo-protocol Connection Closed');
+  });
+  connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+          console.log("Received: '" + message.utf8Data + "'");
       }
-    });
-  } else if (input == '1'){
-    request('http://172.22.6.89/on', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
-      }
-    });
-  }
+  });
+
+  var stdin = process.openStdin();
+  stdin.addListener("data", function(d) {
+    var input = d.toString().trim();
+    if (input == '0'){
+      connection.sendUTF('0');
+    } else if (input == '1'){
+      connection.sendUTF('1');
+    }
+  });
+  
+  // function sendNumber() {
+  //     if (connection.connected) {
+  //         var number = Math.round(Math.random() * 0xFFFFFF);
+  //         connection.sendUTF(number.toString());
+  //         setTimeout(sendNumber, 1000);
+  //     }
+  // }
+  // sendNumber();
 });
+ 
+client.connect('ws://172.22.6.89:3001/');
